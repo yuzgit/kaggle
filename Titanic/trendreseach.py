bde416c3930c1312299c02277313f64d82f6e29a
yuzgit <work.use.address.ofnmk@gmail.com>
2019-06-18 08:37:01 +0900

    一旦csv出力まで実装

--- Titanic/trendreseach.py
+++ Titanic/trendreseach.py
@@ -29,12 +29,11 @@ test  = pd.read_csv('datasets/test.csv')
 
 ## Age特徴(0-100歳とかあまり幅広すぎるとわかりづらいので再設定し可視化)
 # Ageは欠損率が高いので欠損部分はfillnaで-0.5で埋める
-# train['Age'] = train['Age'].fillna(-0.5)
-# test['Age']  = test['Age'].fillna(-0.5)
+train['Age'] = train['Age'].fillna(-0.5)
+test['Age']  = test['Age'].fillna(-0.5)
 
 bins = [-1, 0, 5, 12, 18, 24, 35, 60, np.inf]
 labels = ['Unknown', 'Baby', 'Child', 'Tennager', 'Student', 'Young Adult', 'Adult', 'Senior']
-
 
 train['AgeGroup'] = pd.cut(train['Age'], bins, labels = labels)
 test['AgeGroup']  = pd.cut(test['Age'], bins, labels = labels)
@@ -102,10 +101,14 @@ age_title_mapping = {1: "Young Adult", 2: "Student", 3: "Adult", 4: "Baby", 5: "
 
 # Age Groupの値がない場合TitleからAgeGroupの値を導き代入 (TitleがStudentなら2をAgeGroupへ代入)
 for x in range(len(train["AgeGroup"])):
+    # add
+    train["AgeGroup"][x] = age_title_mapping[train["Title"][x]]
     if train["AgeGroup"][x] == "Unknown":
         train["AgeGroup"][x] = age_title_mapping[train["Title"][x]]
         
 for x in range(len(test["AgeGroup"])):
+    # add
+    test["AgeGroup"][x] = age_title_mapping[test["Title"][x]]
     if test["AgeGroup"][x] == "Unknown":
         test["AgeGroup"][x] = age_title_mapping[test["Title"][x]]
 
@@ -123,3 +126,58 @@ test = test.drop(['Age'], axis = 1)
 ## Name クリーニング
 train = train.drop(['Name'], axis=1)
 test = test.drop(['Name'], axis=1)
+
+## Sex クリーニング 扱いやすいよう数字でマッピング
+sex_mapping = {"male" : 0, "female" : 1}
+train['Sex'] = train['Sex'].map(sex_mapping)
+test['Sex'] = test['Sex'].map(sex_mapping)
+
+## Embarked クリーニング 扱いやすいよう数字でマッピング
+embarked_mapping = {"S" : 1, "C" : 2, "Q" : 3}
+train['Embarked'] = train['Embarked'].map(embarked_mapping)
+test['Embarked'] = test['Embarked'].map(embarked_mapping)
+
+## Fare クリーニング
+for x in range(len(test["Fare"])):
+    if pd.isnull(test["Fare"][x]):
+        pclass = test["Pclass"][x] #Pclass = 3
+        test["Fare"][x] = round(train[train["Pclass"] == pclass]["Fare"].mean(), 4)
+        
+#map Fare values into groups of numerical values
+train['FareBand'] = pd.qcut(train['Fare'], 4, labels = [1, 2, 3, 4])
+test['FareBand'] = pd.qcut(test['Fare'], 4, labels = [1, 2, 3, 4])
+
+#drop Fare values
+train = train.drop(['Fare'], axis = 1)
+test = test.drop(['Fare'], axis = 1)
+
+
+## モデルの選択
+# モデル構築用のテストデータを作る、現在の訓練データの22%をテストデータにする
+from sklearn.model_selection import train_test_split
+
+# 原因不明のNaNが出るので一旦AgeGroupは除外
+predictors = train.drop(['Survived', 'PassengerId'], axis=1)
+print(pd.isnull(predictors).sum())
+terget = train['Survived']
+
+x_train, x_val, y_train, y_val = train_test_split(predictors, terget, test_size = 0.22, random_state = 0)
+
+# Gradient Boosting Classifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
+
+gbk = GradientBoostingClassifier()
+gbk.fit(x_train, y_train)
+y_pred = gbk.predict(x_val)
+acc_gbk = round(accuracy_score(y_pred, y_val) * 100, 2)
+print(acc_gbk)
+
+#set ids as PassengerId and predict survival 
+ids = test['PassengerId']
+
+predictions = gbk.predict(test.drop('PassengerId', axis=1))
+
+#set the output as a dataframe and convert to csv file named submission.csv
+output = pd.DataFrame({ 'PassengerId' : ids, 'Survived': predictions })
+output.to_csv('submission.csv', index=False)
